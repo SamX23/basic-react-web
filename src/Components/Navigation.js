55aa157b22f9180e88f6a3b216440e973b0401d5
SamX23 <skalexsong@gmail.com>
2020-12-02 14:27:39 +0700

    add react-spring

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,32 +1,33 @@
 import { useState } from "react";
+import { useTransition, animated } from "react-spring";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
-  let menu;
-  let menuMask;
-  if (showMenu) {
-    menu = (
-      <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-        The menu
-      </div>
-    );
-    menuMask = (
-      <div
-        className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-        onClick={() => setShowMenu(false)}
-      ></div>
-    );
-  }
+  const transitions = useTransition(showMenu, null, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  // mask className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
 
   return (
     <nav>
       <span className="text-xl">
         <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
       </span>
-      {menuMask}
-      {menu}
+      {transitions.map(({ item, props, key }) => (
+        <animated.div
+          key={key}
+          style={props}
+          className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+        >
+          This is the menu
+        </animated.div>
+      ))}
     </nav>
   );
 }
